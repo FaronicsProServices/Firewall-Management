2abd68159653e5c2f81fe62e8401fe1adeb79246
FaronicsProServices <proservices@faronics.com>
2024-11-21 01:59:12 +0530

    Create DisplayFirewallSettings.ps1

---
+++ DisplayFirewallSettings.ps1
@@ -0,0 +1,3 @@
+# Retrieves and displays the firewall settings for all profiles (Domain, Private, Public) on the local machine.
+# This includes information such as whether the firewall is enabled or disabled for each profile.
+Get-NetFirewallProfile
