4a61aadf0d336875b9d2de008259c207d7f72a20
FaronicsProServices <proservices@faronics.com>
2024-11-21 02:01:40 +0530

    Create ListAllActiveFirewallRules.ps1

---
+++ ListAllActiveFirewallRules.ps1
@@ -0,0 +1,3 @@
+# Retrieves and displays all the firewall rules configured on the local machine, including inbound and outbound rules.
+# This can be used to see which applications and ports are allowed or blocked by the firewall.
+Get-NetFirewallRule
